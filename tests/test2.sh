5aede11ff927b9fb747b104c0d009ce4822d7292
MOTETPAPER <194758925+motetpaper@users.noreply.github.com>
2025-05-31 11:49:06 -0700

    Update test2.sh

--- tests/test2.sh
+++ tests/test2.sh
@@ -11,11 +11,11 @@ msg() {
 pnyn version
 echo
 
-msg '$ pnyn tonemarksiso han2 yu3 pin1 yin1'
-pnyn tonemarksiso han2 yu3 pin1 yin1
+msg '$ pnyn tonemarksiso han4 yu3 pin1 yin1'
+pnyn tonemarksiso han4 yu3 pin1 yin1
 echo
 
 
-msg '$ pnyn tm han2 yu3 pin1 yin1'
-pnyn tm han2 yu3 pin1 yin1
+msg '$ pnyn tm han4 yu3 pin1 yin1'
+pnyn tm han4 yu3 pin1 yin1
 echo
